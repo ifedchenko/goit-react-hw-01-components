75f22c5dbae15aa4497b03621c6f42d6dfea55bf
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-06-25 17:40:28 +0300

    Finish task Transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import user from '../data/user.json';
 import data from '../data/data.json';
+import transactions from '../data/transactions.json';
 
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -15,6 +17,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,41 @@
+import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+export function TransactionHistory({ items }) {
+  return (
+    <div className={css.transactionHistory}>
+      <table className={css.transactionHistoryTable}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => {
+            return (
+              <tr key={id}>
+                <td>{type}</td>
+                <td>{amount}</td>
+                <td>{currency}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
