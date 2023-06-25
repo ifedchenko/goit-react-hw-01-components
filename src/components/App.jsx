13c4da88f241fb992f0c84a9405fcb6140bae7c1
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-06-25 17:22:14 +0300

    Finish task Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import user from '../data/user.json';
+import data from '../data/data.json';
 
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
@@ -12,6 +14,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </>
   );
 };
