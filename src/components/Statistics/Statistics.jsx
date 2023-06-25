a4922e2770637b959c40afffb12c43f1b3a20872
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-06-25 21:51:42 +0300

    bugfix task Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
-import { getRandomHexColor } from 'additional/randomColor.js';
+import { getRandomHexColor } from './randomColor.js';
 
 export function Statistics({ title, stats }) {
   return (
